ddb11299b433ac99b4724895ea1aa20260b6d7c4
mnuradiyat <165825190+nuradiyat@users.noreply.github.com>
2024-04-07 19:58:43 +0700

    memperbaiki sintak
    menambah kan border dashed

--- pertemuan2/table.php
+++ pertemuan2/table.php
@@ -6,35 +6,34 @@
     <title>Document</title>
 </head>
 <body>
-     <table border=1>
-   <tr>
- <th colspan=3 bgcolor="brown">DAFTAR PRODUK</th>
-  </tr>
-  <tr>
-              <td rowspan=4>
-           <img src="pikachu.jpg" height="" width="100" />       </td>
-
-  </tr>
-
-  <tr><td bgcolor="green">Nama Produk</td>
-     
-<td bgcolor="magenta">Canon EOS M10 Kit EF-M 15-45mm</td>
-  </tr>
-  <tr>
+    <table border="1" style="border-style: dashed;">
+      <tr>
+        <th colspan="3" bgcolor="brown">DAFTAR PRODUK</th>
+      </tr>
+      <tr>
+        <td rowspan="4">
+            <img src="pikachu.jpg" height="" width="100" />
+        </td>
+      </tr>
+      <tr>
+        <td bgcolor="green">Nama Produk</td>
+        <td bgcolor="magenta">Canon EOS M10 Kit EF-M 15-45mm</td>
+      </tr>
+      <tr>
         <td bgcolor="green">Harga</td>
         <td bgcolor="magenta">Rp 4.899.000</td>
-  <tr>
+      <tr>
         <td bgcolor="green">Fitur Produk</td>
         <td bgcolor="magenta">
-     <ul>
-    <li>Kamera mirrolesss</li>
-    <li>Efektifitas Piksel : 18MP</li>
-    <li>Tipe sensor: CMOS DIGIC 6</li>
-    <li>Layar : 3.0 Inch</li>
-    <li>Memiliki lampu flash dengan jarak jangkauan hingga 5 meter</li>
-     </ul>
-       </td>
-  </tr>
-  </table>
+          <ul>
+            <li>Kamera mirrolesss</li>
+            <li>Efektifitas Piksel : 18MP</li>
+            <li>Tipe sensor: CMOS DIGIC 6</li>
+            <li>Layar : 3.0 Inch</li>
+            <li>Memiliki lampu flash dengan jarak jangkauan hingga 5 meter</li>
+          </ul>
+        </td>
+      </tr>
+    </table>
 </body>
 </html>
