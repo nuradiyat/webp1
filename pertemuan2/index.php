2332e88cb4e664633a936d54ad6d9f612a791142
mnuradiyat <165825190+nuradiyat@users.noreply.github.com>
2024-04-07 18:56:06 +0700

    pertemuan 2

---
+++ pertemuan2/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $nama = "John";
+    $alamat = "j.l Badak No.2 Cimahi-Bandung";
+    $ttl = "jakarta";
+    $telp = "083256";
+
+    echo "Nama = $nama<br>";
+    echo "Alamat = $alamat<br>";
+    echo "TTL = $ttl<br>";
+    echo "Telpon = $telp<br>";
+    ?>
+</body>
+</html>
---
+++ pertemuan2/table.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <table border=1>
+   <tr>
+ <th colspan=3 bgcolor="brown">DAFTAR PRODUK</th>
+  </tr>
+  <tr>
+              <td rowspan=4>
+           <img src="pikachu.jpg" height="" width="100" />       </td>
+
+  </tr>
+
+  <tr><td bgcolor="green">Nama Produk</td>
+     
+<td bgcolor="magenta">Canon EOS M10 Kit EF-M 15-45mm</td>
+  </tr>
+  <tr>
+        <td bgcolor="green">Harga</td>
+        <td bgcolor="magenta">Rp 4.899.000</td>
+  <tr>
+        <td bgcolor="green">Fitur Produk</td>
+        <td bgcolor="magenta">
+     <ul>
+    <li>Kamera mirrolesss</li>
+    <li>Efektifitas Piksel : 18MP</li>
+    <li>Tipe sensor: CMOS DIGIC 6</li>
+    <li>Layar : 3.0 Inch</li>
+    <li>Memiliki lampu flash dengan jarak jangkauan hingga 5 meter</li>
+     </ul>
+       </td>
+  </tr>
+  </table>
+</body>
+</html>
